e8c1890223985f049919be7b2061195ede3b30a3
H1kkamori <165922020+H1kkamori@users.noreply.github.com>
2024-06-11 19:52:50 +0300

    initial commit

---
+++ Helpers.h
@@ -0,0 +1,11 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+double squareOfSum(double a, double b);
+
+#endif // HELPERS_H
+
+double squareOfSum(double a, double b) {
+    double sum = a + b;
+    return sum * sum;
+}
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main() {
+    double a = 3.0;
+    double b = 4.0;
+    double result = squareOfSum(a, b);
+
+    std::cout << "The square of the sum is: " << result << std::endl;
+    return 0;
+}
